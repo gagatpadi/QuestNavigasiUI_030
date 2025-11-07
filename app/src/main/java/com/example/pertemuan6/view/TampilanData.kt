5d5e7dc971d85eabd41a6f245c514a740a7c0155
gagatpadi <padiyogaradhana@gmail.com>
2025-11-07 16:06:22 +0700

    val

--- app/src/main/java/com/example/pertemuan6/view/TampilanData.kt
+++ app/src/main/java/com/example/pertemuan6/view/TampilanData.kt
@@ -2,11 +2,18 @@ package com.example.pertemuan6.view
 
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import com.example.pertemuan6.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TampilData(
     onBackBtnClick:()-> Unit
 ){
+    val items = listOf(
+        Pair(stringResource(id = R.string.nama_lengkap), "Contoh Nama"),
+        Pair(stringResource(id = R.string.jenis_kelamin), "Lainnya"),
+        Pair(stringResource(id = R.string.alamat), "Yogyakarta"),
+        )
 
 }
