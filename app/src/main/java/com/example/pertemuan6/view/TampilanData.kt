92782f7c8f2540b01533cd5ee62be8d17420202d
gagatpadi <padiyogaradhana@gmail.com>
2025-11-07 16:15:39 +0700

    Scaffold

--- app/src/main/java/com/example/pertemuan6/view/TampilanData.kt
+++ app/src/main/java/com/example/pertemuan6/view/TampilanData.kt
@@ -1,7 +1,14 @@
 package com.example.pertemuan6.view
 
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import com.example.pertemuan6.R
 
@@ -15,5 +22,13 @@ fun TampilData(
         Pair(stringResource(id = R.string.jenis_kelamin), "Lainnya"),
         Pair(stringResource(id = R.string.alamat), "Yogyakarta"),
         )
+    Scaffold (modifier = Modifier,
+        {
+            TopAppBar(
+                title = { Text(text = stringResource(id=R.string.tampil), color = Color.White)},
+                colors = TopAppBarDefaults.mediumTopAppBarColors(colorResource(id=R.color.teal_700))
+            )
+        }){isiRuang->
 
+    }
 }
